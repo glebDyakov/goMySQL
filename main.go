8d3aa36c5e636ce9903545487e92ed2423eb2edd
Rodion <diakovrodion@mail.ru>
2020-12-30 21:42:38 +0300

    go Second Project

---
+++ main.go
@@ -0,0 +1,41 @@
+﻿package main
+import(
+	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+type User struct {
+	Name string `json:"name"`
+	Age uint16 `json:"age"`
+}
+func main(){
+	
+	db, err := sql.Open("mysql", "mysql:mysql@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	// insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES('Bob', 35)")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer insert.Close()
+	
+	res, err := db.Query("SELECT `name`, `age` FROM `users`")
+	if err != nil {
+		panic(err)
+	}
+	for res.Next(){
+		var user User
+		err = res.Scan(&user.Name, &user.Age)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
+	}
+
+	fmt.Println("Подключено к MySQL")
+
+}
